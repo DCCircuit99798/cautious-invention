9a6e167ce054121fdf37638503de71d1267a04f2
DCCircuit99798 <st21035@ormiston.school.nz>
2023-07-23 22:48:09 +1200

    trying to get non-pitch audio rates to work, pyrubberband import still not working

--- uprate_audio_nopitch.py
+++ uprate_audio_nopitch.py
@@ -1,5 +1,7 @@
 # import the required modules
 from pydub import AudioSegment
+import soundfile as sf
+import pyrubberband # WARNING: import broken. this file doesn't work for now
 
 # ask for user info
 title = input("Enter file title: ")
@@ -16,12 +18,25 @@ except FileNotFoundError:
     input()
     exit()
 
-# create a version of the audio file with a different playback speed
-audio_rate = audio.speedup(playback_speed=rate)
+# export audio file as .wav file
+# (pyrubberband can only work with .wav files)
+audio.export(title + '.wav', format='wav')
+
+
+# open .wav file
+y, sr = sf.read(title + '.wav')
+
+# change pitch alongside playback speed
+y_speed = pyrubberband.time_stretch(y, sr, rate)
+y_pitch = pyrubberband.pitch_shift(y, sr, rate)
+
+# export .wav file with the altered playback speed
+sf.write(('new.' + title + '.wav'), y_stretch, sr, format='wav')
+
+
+# open .wav file with altered speed using AudioSegment
+audio_rate_wav = AudioSegment.from_file('new.' + title + '.wav')
 
 # export the file
 # (filename comes from file "title" and the chosen rate)
 audio_rate.export(title + ".audio_" + str(rate) + "x.ogg", format="ogg")
-
-
-
