019ec92c9ffb5a16f4840e08758ede2b6beb8bbe
DCCircuit99798 <st21035@ormiston.school.nz>
2023-08-26 21:34:21 +1200

    fixed program crashing if fields in level.json were null

--- icup.py
+++ icup.py
@@ -500,8 +500,27 @@ class App(tk.Tk):
 
                 # check if level.json has id and title
                 # (required to be able to play in-game)
-                self.user_json['id']
-                self.user_json['title']
+                if (self.user_json['id'] == None or
+                    self.user_json['title'] == None):
+                    messagebox.showerror(
+                        'Error',
+                        'level.json file within Cytoid level file is missing ' \
+                        'required keys (id, title)'
+                        )
+
+                    self.level_validity = False
+
+                    # configure 'choose file' button border to red
+                    self.style.configure(
+                        'ChooseFile.WidgetBorder.TFrame',
+                        background='#ee9f9f')
+
+                    # remove the extracted level.json file
+                    os.remove('level.json')
+
+                    # prevent the rest of the level validity checks
+                    # from running
+                    return
 
         # if level.json file is invalid JSON, display error messagebox
         # and mark Cytoid level as invalid
@@ -1575,8 +1594,9 @@ class App(tk.Tk):
                               + str(self.current_rate)
                               + 'x]')
 
-                    # if the chart has no title_localized, skip this step
-                    except KeyError:
+                    # if the chart has no title_localized or
+                    # title_localized is set to null, skip this step
+                    except (KeyError, TypeError) as e:
                         pass
 
                     if self.easy_music_path != None:
@@ -1710,8 +1730,9 @@ class App(tk.Tk):
                               + str(self.current_rate)
                               + 'x]')
 
-                    # if the chart has no title_localized, skip this step
-                    except KeyError:
+                    # if the chart has no title_localized or
+                    # title_localized is set to null, skip this step
+                    except (KeyError, TypeError) as e:
                         pass
 
                     if self.hard_music_path != None:
@@ -1845,8 +1866,9 @@ class App(tk.Tk):
                               + str(self.current_rate)
                               + 'x]')
 
-                    # if the chart has no title_localized, skip this step
-                    except KeyError:
+                    # if the chart has no title_localized or
+                    # title_localized is set to null, skip this step
+                    except (KeyError, TypeError) as e:
                         pass
 
                     if self.ex_music_path != None:
